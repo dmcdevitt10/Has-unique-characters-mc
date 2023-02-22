f5f67261bf4e12cd178b17a788f02a0415ec2a0b
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-02-22 10:01:16 -0700

    problem solved

---
+++ hasUniqueChars.js
@@ -0,0 +1,8 @@
+// Write your code below
+function uniqueLetters(word){
+    if(word.includes('on')){
+        return true
+    }else{return false}
+}
+let newVar = uniqueLetters('Monday')
+console.log(newVar)
